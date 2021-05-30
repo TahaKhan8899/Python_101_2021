b04f1e2ca1e407405358c432c75b0326dff2a036
Taha Zeeshan Khan <khant21@mcmaster.ca>
2021-05-30 11:23:06 -0400

    cleaned up strings

--- variables/strings.py
+++ variables/strings.py
@@ -6,25 +6,37 @@ username2 = "player2"
 #  "p"  "l"  "a"  "y"  "e"  "r"  "1"
 print(username1[3])
 
+# STRING OPERATIONS
+
+# lower
 print(username1.lower())
 
+# len
 print(len(username1))
 
+# index of a specific character
 print(username1.index('y'))
 
-# username1 = username1.replace('p', 'c')
-# print(username1)
+# replace 1 character with another
+username1 = username1.replace('p', 'c')
+print(username1)
 
+# concatenate (combine) strings
 sentence = username1 + " beat " + username2
-
 print(sentence)
 
 # SLICING
 # Syntax: stringVariable[start:end]
 # take all the letter from 2 to 5 (5 not included)
 
-slicedName = username1[:6]
-print(slicedName)
+slicedName1 = username1[2:6]
+print(slicedName1)
+
+slicedName2 = username1[:4]
+print(slicedName2)
+
+slicedName3 = username1[2:]
+print(slicedName3)
 
 jumpedThing = "fence"
 jumpedThing = jumpedThing[0:2] + jumpedThing[2].upper() + jumpedThing[3:]

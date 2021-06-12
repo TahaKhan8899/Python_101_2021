02c53000d33e42701797dafd3dc9ed641ed8a373
Taha Zeeshan Khan <khant21@mcmaster.ca>
2021-06-12 11:12:17 -0400

    Added class 5 code

--- loops/loops.py
+++ loops/loops.py
@@ -1,5 +1,5 @@
-# Purpose of a loop
-jobs = ["job1", "job2", "job3", "job4", "job5"]
+# # Purpose of a loop
+jobs = ["job1", "job2", "job3", "job4", "job5", "job6", "job7"]
 # print("processing " + jobs[0])
 # print("processing " + jobs[1])
 # print("processing " + jobs[2])
@@ -31,6 +31,57 @@ for x in range(10, 0, -1):
     print(x)
 
 # loop through characters in a string
-x = "hello"
+x = "hello my name is taha"
 for char in x:
     print(char)
+
+# loop through dictionaries
+scores = {"user143": 10, "player_2": 8, "user9323": 13, "3nnk92": 7}
+for key in scores:
+    print(key)
+    print(scores[key])
+
+# we want to print 1a 1b 1c, 2a 2b 2c .....
+list1 = [1, 2, 3, 4, 5]
+list2 = ['a', 'b', 'c']
+for nums in list1:
+    for char in list2:
+        print(str(nums) + char)
+
+# while loop with a finite counter
+counter = 0
+while counter < 10000:
+    print("taha " + str(counter))
+    counter += 1
+
+# infinite loop with a condition
+running = True
+while running:
+    print("yay my game is running")
+
+# a while loop with a counter and a condition
+running = True
+score = 0
+while running:
+    if score == 10:
+        # both ways stop the loop
+        break
+        running = False
+    else:
+        print(score)
+        score += 1
+print("game over")
+
+
+# combining dictionaries
+dict1 = {'Taha': 5, 'Uzair': 7, 'Sarah': 10}
+dict2 = {'Abdul Rahman': 8, 'Umar': 20, 'Mark': 11}
+
+# we basically want to do this for all the key value pairs in dict2:
+dict1["Zaid"] = 2
+
+for key in dict2:
+    dict1[key] = dict2[key]
+print(dict1)
+
+

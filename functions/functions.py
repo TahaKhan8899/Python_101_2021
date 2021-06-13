2c0540427a43c60df12037332530943979bace77
Taha Zeeshan Khan <khant21@mcmaster.ca>
2021-06-13 11:12:58 -0400

    added comments to functions file

--- functions/functions.py
+++ functions/functions.py
@@ -1,41 +1,55 @@
-def sayHello(fname, lname):
+# Simplest function
+def sayHello():
+    print("hi!")
+
+
+# We need to call functions AFTER we define them (defined above)
+sayHello()
+
+
+# function with parameters
+def sayHelloTo(fname, lname):
     print("Hi " + fname + " " + lname)
 
 
-# sayHello("Taha", "Khan")
+sayHelloTo("taha", "khan")
 
 
+# Function with parameters and a returns value
 def addNumbers(a, b):
     sum = a + b
     return sum
 
 
-# answer1 = addNumbers(2, 3)
-# answer2 = addNumbers(4, 7)
-# answer3 = addNumbers(10, 76)
-# print(answer1)
-# print(answer2)
-# print(answer3)
+answer1 = addNumbers(2, 3)
 
-def areaOfRect(l, w):
+# They can be called multiple times with different PARAMETERS
+# This is nice because we don't need to keep re-writing the code for adding numbers, we write it ONCE in our function
+# and call that function whenever we want
+answer2 = addNumbers(5, 10)
+answer3 = addNumbers(2, 15)
+print(answer1)
+print(answer2)
+print(answer3)
+
+def areaRect(l, w):
     return l * w
 
-# area1 = areaOfRect(2, 4)
-# print(area1)
 
+# try this #1:
 def printNums():
-    for x in range(-10, 0):
-        print(x)
+    for i in range(-10, 0):
+        print(i)
 
-# printNums()
 
+# Try this #2:
 def userSum():
     userNum = int(input("enter a number: "))
     sum = 0
-    for num in range(1, userNum+1):
-        sum += num
-
+    for x in range(1, userNum + 1):
+        sum += x
     return sum
 
-sum1 = userSum()
-print(sum1)
+
+result = userSum()
+print(result)

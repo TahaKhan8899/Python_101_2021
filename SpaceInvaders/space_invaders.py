1185d752bfb7072d31501c511a6456e179257d77
Taha Zeeshan Khan <khant21@mcmaster.ca>
2021-06-26 11:13:28 -0400

    Update space invaders with class 9

--- SpaceInvaders/space_invaders.py
+++ SpaceInvaders/space_invaders.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -14,14 +15,40 @@ playerX = 370
 playerY = 480
 playerX_Change = 0
 
+# load enemy image and assign other variable for it
+enemyImg = pygame.image.load('/Users/tahakhan/PycharmProjects/python101/SpaceInvaders/images/enemy.png')
+enemyX = random.randint(0, 800)
+enemyY = random.randint(50, 150)
+enemyX_Change = 3
+enemyY_Change = 40
+
+# load bullet image and assign other variable for it
+bulletImg = pygame.image.load('/Users/tahakhan/PycharmProjects/python101/SpaceInvaders/images/bullet.png')
+bulletX = 0
+bulletY = 0
+bulletY_Change = 40
+bulletState = 0
+
 
 def draw_player(x, y):
     screen.blit(playerImg, (x, y))
 
 
+def draw_enemy(x, y):
+    screen.blit(enemyImg, (x, y))
+
+
+def fire_bullet(x, y):
+    global bulletState
+    bulletState = 1
+    screen.blit(bulletImg, (x + 16, y + 16))
+
+
 # This is the game loop
 running = True
 while running:
+
+    # this is the event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -31,6 +58,10 @@ while running:
                 playerX_Change = -8
             if event.key == pygame.K_RIGHT:
                 playerX_Change = 8
+            if event.key == pygame.K_SPACE:
+                bulletX = playerX
+                bulletY = playerY
+                fire_bullet(bulletX, bulletY)
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 playerX_Change = 0
@@ -40,7 +71,23 @@ while running:
 
     # change playerX by the playerX_Change amount and draw it
     playerX += playerX_Change
+    if playerX <= 0:
+        playerX = 0
+    elif playerX > 736:
+        playerX = 736
     draw_player(playerX, playerY)
+
+    # move enemy X and check boundaries
+    enemyX += enemyX_Change
+    if enemyX <= 0 or enemyX > 736:
+        enemyX_Change *= -1
+        enemyY += enemyY_Change
+    draw_enemy(enemyX, enemyY)
+
+    # keep drawing the bullet and moving it upwards if it's in the fire state
+    if bulletState == 1:
+        bulletY -= bulletY_Change
+        fire_bullet(bulletX, bulletY)
 
     # Must be called to refresh the screen
     pygame.display.update()
